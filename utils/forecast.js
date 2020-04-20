ad1fdc49248987db160a708c70468f5adea9a872
ahmed michrgui <ahmed.michrgui@hotmail.fr>
2020-04-20 14:48:25 +0100

    init commit

---
+++ utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request');
+
+const forecast = function(latitude,longitude,callback) {
+    const urlDarkSky = 'https://api.darksky.net/forecast/ad95e11dbf8fe6961a05425dea338c24/'+latitude+','+longitude+'?units=si';
+    request({url:urlDarkSky,json:true},(error, {body})=>{
+        if (error){
+            callback('unable to connect to weather services!',undefined);
+        } else if (body.error){
+            callback(body.error,undefined);
+        } else {
+            const weatherNow = body.currently;
+            const weatherToday = body.daily.data[0].summary;
+            forecastString = weatherToday + ' It is currently ' + weatherNow.temperature + ' degrees out . There is ' +
+                weatherNow.precipProbability + ' chance of rain'
+        callback(undefined,forecastString);
+        }
+    });
+}
+
+module.exports = forecast;
